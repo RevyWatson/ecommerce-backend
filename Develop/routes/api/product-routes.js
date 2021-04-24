27d947adff93c4d8f995369bbe9b93fa414e817b
RevyWatson <76264693+RevyWatson@users.noreply.github.com>
2021-04-24 17:10:58 -0400

    Updated routes js files.

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -5,7 +5,7 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 
 // get all products
 // find all products
-// be sure to include its associated Category and Tag data
+// associated Category and Tag data
 router.get('/', async (req, res) => {
   try {
     const productData = await Product.findAll({ include: [Category, { model: Tag, through: ProductTag }] });
@@ -16,19 +16,19 @@ router.get('/', async (req, res) => {
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+// find a single product by its `id`
+// associated Category and Tag data
+router.get('/:id', async (req, res) => {
   try {
     const productData = await Product.findOne({ where: { id: req.params.id }, include: [Category, { model: Tag, through: ProductTag }] });
     res.status(200).json(productData);
   } catch (error) {
     res.status(500).json(err);
   }
-  // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
 });
 
 // create new product
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   /* req.body should look like this...
     {
       product_name: "Basketball",
@@ -60,7 +60,7 @@ router.post('/', (req, res) => {
 });
 
 // update product
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update product data
   Product.update(req.body, {
     where: {
@@ -101,8 +101,24 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
-  // delete one product by its `id` value
+// delete one product by its `id` value
+router.delete('/:id', async (req, res) => {
+  try {
+    const productData = await Product.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+
+    if (!productData) {
+      res.status(404).json({ message: 'No Product found with this id!' });
+      return;
+    }
+
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;

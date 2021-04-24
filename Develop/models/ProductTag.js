ed900d6fbbaca5db98b49116845809c2adbf5e30
RevyWatson <76264693+RevyWatson@users.noreply.github.com>
2021-04-23 22:34:33 -0400

    Updated js files, readme, gitignore and removed dotenv from repo.

--- Develop/models/ProductTag.js
+++ Develop/models/ProductTag.js
@@ -2,11 +2,31 @@ const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection');
 
-class ProductTag extends Model {}
+class ProductTag extends Model { }
 
+//fields and rules for ProductTag model
 ProductTag.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      refernces: {
+        model: 'product',
+        key: 'id'
+      }
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      refernces: {
+        model: 'tag',
+        key: 'id'
+      }
+    },
   },
   {
     sequelize,
--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,12 +5,28 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+
+})
 
 // Categories have many Products
+Category.hasMany(Product, {
+
+})
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+through: {
+  model: ProductTag,
+}
+})
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+through: {
+  model: ProductTag,
+}
+})
 
 module.exports = {
   Product,
@@ -18,3 +34,12 @@ module.exports = {
   Tag,
   ProductTag,
 };
+
+
+// * `Product` belongs to `Category`, and `Category` has many `Product` models, as a category can have multiple products 
+//but a product can only belong to one category.
+
+// * `Product` belongs to many `Tag` models, and `Tag` belongs to many `Product` models. Allow products to have multiple 
+//tags and tags to have many products by using the `ProductTag` through model.
+
+// > **Hint:** Make sure you set up foreign key relationships that match the column we created in the respective models.
